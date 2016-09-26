f1f7b353f9ed63f8fe98f97ffa548b50e3f29328
Ben Marshall <me@benmarshall.me>
2016-09-26 10:03:21 -0500

    Added all available slick setting options.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -128,70 +128,136 @@ function slickity_slideshow_shortcode( $atts )
               $(function() {
                 $( '#slickity-<?php echo $a['id']; ?>' ).slick({
                   <?php if ( get_field( 'slickity_accessibility' ) ) { ?>
-                    accessibility:  true,
+                    accessibility: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_adaptive_height' ) ) { ?>
-                    adaptiveHeight:  true,
+                    adaptiveHeight: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_autoplay' ) ) { ?>
-                    autoplay:  true,
+                    autoplay: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_autoplay_speed' ) ) { ?>
                     autoplaySpeed:  <?php echo get_field( 'slickity_autoplay_speed' ); ?>,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_arrows' ) ) { ?>
-                    arrows:  true,
+                    arrows: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_as_nav_for' ) ) { ?>
-                    asNavFor:  '<?php echo get_field( 'slickity_as_nav_for' ); ?>',
+                    asNavFor: '<?php echo get_field( 'slickity_as_nav_for' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_append_arrows' ) ) { ?>
-                    appendArrows:  '<?php echo get_field( 'slickity_append_arrows' ); ?>',
+                    appendArrows: '<?php echo get_field( 'slickity_append_arrows' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_append_dots' ) ) { ?>
-                    appendDots:  '<?php echo get_field( 'slickity_append_dots' ); ?>',
+                    appendDots: '<?php echo get_field( 'slickity_append_dots' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_previous_arrow' ) ) { ?>
-                    prevArrow:  '<?php echo get_field( 'slickity_previous_arrow' ); ?>',
+                    prevArrow: '<?php echo get_field( 'slickity_previous_arrow' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_next_arrow' ) ) { ?>
-                    nextArrow:  '<?php echo get_field( 'slickity_next_arrow' ); ?>',
+                    nextArrow: '<?php echo get_field( 'slickity_next_arrow' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_center_mode' ) ) { ?>
-                    centerMode:  true,
+                    centerMode: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_center_padding' ) ) { ?>
-                    centerPadding:  '<?php echo get_field( 'slickity_center_padding' ); ?>',
+                    centerPadding: '<?php echo get_field( 'slickity_center_padding' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_css_ease' ) ) { ?>
-                    cssEase:  '<?php echo get_field( 'slickity_css_ease' ); ?>',
+                    cssEase: '<?php echo get_field( 'slickity_css_ease' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_dots' ) ) { ?>
-                    dots:  true,
+                    dots: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_dots_class' ) ) { ?>
-                    dotsClass:  '<?php echo get_field( 'slickity_dots_class' ); ?>',
+                    dotsClass: '<?php echo get_field( 'slickity_dots_class' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_draggable' ) ) { ?>
-                    draggable:  true,
+                    draggable: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_fade' ) ) { ?>
-                    fade:  true,
+                    fade: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_focus_on_select' ) ) { ?>
-                    focusOnSelect:  true,
+                    focusOnSelect: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_easing' ) ) { ?>
-                    easing:  '<?php echo get_field( 'slickity_easing' ); ?>',
+                    easing: '<?php echo get_field( 'slickity_easing' ); ?>',
                   <?php } ?>
                   <?php if ( get_field( 'slickity_edge_friction' ) ) { ?>
-                    edgeFriction:  <?php echo get_field( 'slickity_edge_friction' ); ?>,
+                    edgeFriction: <?php echo get_field( 'slickity_edge_friction' ); ?>,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_infinite' ) ) { ?>
-                    infinite:  true,
+                    infinite: true,
                   <?php } ?>
                   <?php if ( get_field( 'slickity_lazy_load' ) ) { ?>
-                    lazyLoad:  '<?php echo get_field( 'slickity_lazy_load' ); ?>',
+                    lazyLoad: '<?php echo get_field( 'slickity_lazy_load' ); ?>',
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_mobile_first' ) ) { ?>
+                    mobileFirst: true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_pause_on_focus' ) ) { ?>
+                    pauseOnFocus: true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_pause_on_hover' ) ) { ?>
+                    pauseOnHover: true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_pause_on_dots_hover' ) ) { ?>
+                    pauseOnDotsHover: true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_respond_to' ) ) { ?>
+                    respondTo: '<?php echo get_field( 'slickity_respond_to' ); ?>',
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_rows' ) ) { ?>
+                    rows:  <?php echo get_field( 'slickity_rows' ); ?>,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_slides_per_row' ) ) { ?>
+                    slidesPerRow:  <?php echo get_field( 'slickity_slides_per_row' ); ?>,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_slides_to_show' ) ) { ?>
+                    slidesToShow:  <?php echo get_field( 'slickity_slides_to_show' ); ?>,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_slides_to_scroll' ) ) { ?>
+                    slidesToScroll:  <?php echo get_field( 'slickity_slides_to_scroll' ); ?>,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_speed' ) ) { ?>
+                    speed:  <?php echo get_field( 'slickity_speed' ); ?>,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_swipe' ) ) { ?>
+                    swipe:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_swipe_to_slide' ) ) { ?>
+                    swipeToSlide:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_touch_move' ) ) { ?>
+                    touchMove:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_touch_threshold' ) ) { ?>
+                    touchThreshold:  <?php echo get_field( 'slickity_touch_threshold' ); ?>,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_use_css' ) ) { ?>
+                    useCSS:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_use_transform' ) ) { ?>
+                    useTransform:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_variable_width' ) ) { ?>
+                    variableWidth:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_vertical' ) ) { ?>
+                    vertical:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_vertical_swiping' ) ) { ?>
+                    verticalSwiping:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_right-to-left' ) ) { ?>
+                    rtl:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_wait_for_animate' ) ) { ?>
+                    waitForAnimate:  true,
+                  <?php } ?>
+                  <?php if ( get_field( 'slickity_z-index' ) ) { ?>
+                    zIndex:  <?php echo get_field( 'slickity_z-index' ); ?>,
                   <?php } ?>
                 });
               });
